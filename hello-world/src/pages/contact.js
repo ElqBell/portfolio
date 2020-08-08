03ee2d565e068fe96099098205f2f85a686fce79
ElqBell <silenc8@gmail.com>
2020-08-08 15:00:34 +0300

    created initial contact and projects page

---
+++ hello-world/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function Home() {
+  return (
+    <div style={{ color: 'lightblue' }}>
+      <h1>Sorry, but this page doesn't exist.</h1>
+      <Link to="/">Go to home page</Link>
+    </div>
+  );
+}
