e9e1ec511fc9ea72ce3d8a0539fbf64a1b573564
ElqBell <silenc8@gmail.com>
2020-08-14 14:53:24 +0300

    created 'about' page style

--- hello-world/src/pages/about/index.js
+++ hello-world/src/pages/about/index.js
@@ -4,30 +4,31 @@ import Layout from "../../components/layout/layout";
 import localStyles from "./about.module.css";
 
 export default function About() {
+  console.log(localStyles);
   return (
     <Layout mainClass={localStyles.mainContent}>
       <section>
         <h1 className="heading-1">Hello, my name is Deimantas Butėnas.</h1>
-        <article>
-          <h3 className="heading-3">I'm a passionate frontend developer and designer with high attention to detail and user experience. Currently I'm studying software engineering at Kaunas University of Technology.</h3>
-        </article>
-        <article>
-          <h2 className="heading-2">My skills:</h2>
-          <ul>
-            <li>ReactJS</li>
-            <li>JavaScript</li>
-            <li>SCSS(CSS)</li>
-            <li>HTML</li>
-            <li>Website design</li>
-          </ul>
-        </article>
-        <article>
-          <h2 className="heading-2">I also have some experience with:</h2>
-          <ul>
-            <li>NodeJS</li>
-            <li>Amazon Web Services</li>
-          </ul>
-        </article>
+        <h3 className="heading-3">I'm a passionate frontend developer and designer with high attention to detail and user experience. Currently I'm studying software engineering at Kaunas University of Technology.</h3>
+        <div className={localStyles.articleWrapper}>
+          <article>
+            <h2 className="heading-2">Tools I use:</h2>
+            <ul>
+              <li>ReactJS</li>
+              <li>JavaScript</li>
+              <li>SCSS(CSS)</li>
+              <li>HTML</li>
+              <li>Git</li>
+            </ul>
+          </article>
+          <article>
+            <h2 className="heading-2">I also have some experience with:</h2>
+            <ul>
+              <li>NodeJS</li>
+              <li>Amazon Web Services</li>
+            </ul>
+          </article>
+        </div>
       </section>
       <section>
         <h2 className="heading-2">Interested?</h2>
