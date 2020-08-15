83aed0d4bcea9900859e8cb4e7eee0dc4b60ccfa
ElqBell <silenc8@gmail.com>
2020-08-15 16:16:25 +0300

    finished 'contact' page style + fixed 'about' page style

--- hello-world/src/pages/contact/index.js
+++ hello-world/src/pages/contact/index.js
@@ -66,11 +66,7 @@ export default function Contact() {
       <aside>
         <h2 className="heading-2">Or if you'd rather</h2>
         <h2 className="heading-2">contact me directly:</h2>
-        <h2 className="heading-2">
-          <a href="mailto:deimantas.butenas@gmail.com">
-            deimantas.butenas@gmail.com
-          </a>
-        </h2>
+        <a href="mailto:deimantas.butenas@gmail.com">deimantas.butenas<p>@gmail.com</p></a>
         <img src={svgImage} onError={function(){this.src=fallbackImage}} alt="" />
       </aside>
     </Layout>
