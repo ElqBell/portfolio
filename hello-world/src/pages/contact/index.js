29a1bca1493d49bd63d550273b08aa9c3d904554
ElqBell <silenc8@gmail.com>
2020-08-15 14:52:57 +0300

    created post data request code for email

--- hello-world/src/pages/contact/index.js
+++ hello-world/src/pages/contact/index.js
@@ -4,12 +4,53 @@ import svgImage from "../../images/svg/email-send-letter.svg";
 import fallbackImage from "../../images/svg/gallery-img.jpg";
 import localStyles from "./contact.module.css";
 
+function submitToAPI(e) {
+  e.preventDefault();
+  const url = 'https://3jxqp8oiza.execute-api.eu-central-1.amazonaws.com/beta/contact/';
+  
+  const data = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      msg: document.getElementById("message").value
+  };
+  
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      //'Cross-Domain': 'true'
+    }
+  };
+  
+  // fetch(url, options)
+  //   .then(function(res){return res.json()})
+  //   .then(function(res){console.log(res)});
+
+  // $.ajax({
+  //     type: "POST",
+  //     url : "https://3jxqp8oiza.execute-api.eu-central-1.amazonaws.com/beta/contact/",
+  //     crossDomain: "true",
+  //     contentType: "application/json; charset=utf-8",
+  //     data: JSON.stringify(data),
+  //     success: function () {
+  //       document.getElementById("contact-form").reset();
+  //       document.getElementById("email-submit-button").blur();
+  //     },
+  //     error: function () {
+  //       document.getElementById("contact-form").reset();
+  //       document.getElementById("email-submit-button").blur();
+  //       alert("Something went wrong. Try again or use another contact method.")
+  //     }
+  // });
+}
+
 export default function Contact() {
   return (
     <Layout mainClass={localStyles.mainContent}>
       <section>
         <h1 className="heading-1">Send me a message</h1>
-        <form onSubmit="">
+        <form id="contact-form" onSubmit={submitToAPI}>
           <div>
             <label htmlFor="name">Name</label>
             <label htmlFor="email">Email</label><br/>
@@ -19,7 +60,7 @@ export default function Contact() {
           <label htmlFor="message">Message</label>
           <textarea type="text" id="message" name="message" required />
 
-          <button type="submit" className="button">Submit</button>
+          <button type="submit" id="email-submit-button" className="button">Submit</button>
         </form>
       </section>
       <aside>
