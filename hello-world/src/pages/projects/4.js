41e9657b1b994cd8ea50d83726f67b8ec8d21fdf
ElqBell <silenc8@gmail.com>
2020-08-16 19:39:33 +0300

    removing unnecessary html

--- hello-world/src/pages/projects/4.js
+++ hello-world/src/pages/projects/4.js
@@ -10,30 +10,19 @@ export default function Project4() {
         <section>
           <Link to=".." className={`button ${localStyles.linkBack}`}>Go back</Link>
           <p className={localStyles.projectNumber}>04.</p>
-          <h1 className="heading-1">Project name here</h1>
+          <h1 className="heading-1">Project name.</h1>
           <article>
             <h2 className="heading-3">About:</h2>
-            <p>A photography portfolio website.</p>
+            <p>Description.</p>
           </article>
           <article>
             <h2 className="heading-3">Features:</h2>
             <ul>
-              <li>Image and video lazyloading</li>
-              <li>Automatic image compression</li>
-              <li>Automatic image clean-up(deletion) from S3</li>
-              <li>Automatic 'videos' page HTML code generation</li>
-              <li>Automatic HTML code generation of individual image galleries' pages and 'all-galleries' page</li>
-              <li>Email contact form</li>
             </ul>
           </article>
           <article>
             <h2 className="heading-3">Tools used:</h2>
             <ul>
-              <li>Amazon Web Services(AWS)</li>
-              <li>NodeJS with AWS Lambda</li>
-              <li>JavaScript</li>
-              <li>SCSS</li>
-              <li>HTML</li>
             </ul>
           </article>
         </section>
