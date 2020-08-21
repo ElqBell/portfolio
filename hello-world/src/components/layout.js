7332f18955775908260d810dcb7ef1cde140ddb3
ElqBell <silenc8@gmail.com>
2020-08-21 16:18:44 +0300

    using html.js to edit head because react-helmet didn't work with og meta tags

--- hello-world/src/components/layout.js
+++ hello-world/src/components/layout.js
@@ -17,15 +17,6 @@ export default function Layout(props) {
   return (
     <div className="page-content">
         <Helmet>
-          <meta name="description" content="A frontend development portfolio website of Deimantas Butėnas where you can see his work."/>
-          <meta name="keywords" content="web development, frontend, portfolio, design, web design" />
-          <meta property="og:title" content="Frontend developer Deimantas Butėnas" />
-          <meta property="og:type" content="website" />
-          <meta property="og:description" content="A portfolio website of Deimantas Butėnas where you can see his work." />
-          <meta property="og:image" content="meta-og-image.png" />
-          <meta property="og:locale" content="en_US" />
-          <meta property="og:url" content="https://www.deimantasbutenas.lt/" />
-          <link rel="canonical" href="https://www.deimantasbutenas.lt/" />
           <title>{`DMD - Home`}</title>
         </Helmet>
         <Header/>
